09f4f825ac34da6eb2bbe8e8b59f91c4363a4dec
Shivansh Shukla <41468555+Iam5shiva@users.noreply.github.com>
2021-09-29 00:37:22 +0530

    Updated settings.py

--- Django_Rest_API/settings.py
+++ Django_Rest_API/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xk#lwss8_vv=de!#6$v7nzq#iq04_=%$=yt0w6*j7(^t1l)86t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
